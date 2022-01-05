91baeab6dac56f2a63bb018cf07ed3b2a3128686
Utirna Examination Unit <utirna.akola@gmail.com>
2022-01-05 11:18:35 +0530

    Course Updated

---
+++ Node_NPM/Day-1/npm command.js
@@ -0,0 +1,34 @@
+/*
+    npm node package manager 
+    npx node package executor
+    create package.json
+        npm init
+        npm init -y
+
+    local installation
+        npm install package_name
+        npm i package_name
+
+    global installation
+        npm install package_name -g
+        npm i package_name -g
+
+
+    local uninstallation
+        npm uninstall package_name
+        npm un package_name  
+
+    global uninstallation
+        npm uninstall package_name -g
+        npm un package_name  -g
+
+    local update
+        npm update package_name
+        npm update 
+
+    global update
+        npm update package_name -g
+        npm update -g
+        
+
+*/
